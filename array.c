4cda20914cb6e9277a0d45fa7cf6b764c14c12dd
Rohan Singh <117467200+rohan4693@users.noreply.github.com>
2023-08-30 15:09:07 +0530

    Update array.c

--- array.c
+++ array.c
@@ -30,6 +30,8 @@
 //         {
 //             scanf("%d", &class[i][j]);
 //         }
+
+
         
 //     }
 //     for (int i = 0; i < 2; i++)
@@ -95,6 +97,72 @@
 //         {
 //             c[i][j]=a[i][j]+ b[i][j];
 //             printf("%d ",c[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
+
+
+
+//multiplying two matrix
+// #include<stdio.h>
+// int main()
+// {
+//     int a[3][3], b[3][3],c[3][3], i, j, k,sum;
+//     printf("\n enter the first matrix=");
+//     for(i=0; i<3; i++)
+//     {
+//         for(j=0; j<3; j++)
+//         {
+//             scanf("%d",&a[i][j]);
+//         }
+//     }
+//     printf("\n enter the second matrix=");
+//     for(i=0; i<3; i++)
+//     {
+//         for(j=0; j<3; j++)
+//         {
+//             scanf("%d",&b[i][j]);
+//         }
+//     }
+//     printf("first matrix= \n");
+//     for(i=0; i<3; i++)
+//     {
+//         for(j=0; j<3; j++)
+//         {
+//             printf("%d\t",a[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     printf("second matrix= \n");
+//     for(i=0; i<3; i++)
+//     {
+//         for(j=0; j<3; j++)
+//         {
+//             printf("%d\t",b[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     printf("multiplied matrix=\n");
+//     for(i=0; i<3; i++)
+//     {
+//         for(j=0; j<2; j++)
+//         {
+//             sum=0;
+//             for(k=0;k<3;k++)
+//             {
+//                 sum=sum+a[i][k]*b[j][k];
+//                 c[i][j]=sum;
+//             }
+//         }
+//         printf("\n");
+//     }
+//     for(i=0; i<3;i++)
+//     {
+//         for(j=0; j<3; j++)
+//         {
+//             printf("%d\t",c[i][j]);
 //         }
 //         printf("\n");
 //     }
