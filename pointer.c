4daf016386f46c9353edc304924d8a97efb14651
Rohan Singh <117467200+rohan4693@users.noreply.github.com>
2023-09-02 12:01:04 +0530

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -1,14 +1,14 @@
-#include<stdio.h>
-int main()
-{
-    int a=10 , b=30;
-    int *p, *q;
-    *p=20;
-    p=&a; q=&b;
-    printf("a=%d\nb=%d", *p,*q);
-    printf("\n %d \n %d",p,q);
-    printf("\n  a=%x \n  b=%x",&a,&b);
-    printf("\n%d",a);
-    printf("\n%x",p);
-    return 0;
-}
+// #include<stdio.h>
+// int main()
+// {
+//     int a=10 , b=30;
+//     int *p, *q;
+//     *p=20;
+//     p=&a; q=&b;
+//     printf("a=%d\nb=%d", *p,*q);
+//     printf("\n %d \n %d",p,q);
+//     printf("\n  a=%x \n  b=%x",&a,&b);
+//     printf("\n%d",a);
+//     printf("\n%x",p);
+//     return 0;
+// }
