f18af38a38045caffca3a457ea569a81e8fa9e98
Rohan Singh <117467200+rohan4693@users.noreply.github.com>
2023-09-05 22:58:26 +0530

    Update pointer.c

--- pointer.c
+++ pointer.c
@@ -12,3 +12,30 @@
 //     printf("\n%x",p);
 //     return 0;
 // }
+
+//pointer assigment 
+// #include<stdio.h>
+// int main()
+// {
+//     int a=10, b=3030;
+//     int *p,*q;
+//     p=&a;
+//     q=&b;
+    
+//     printf("%d\n %d\n %d\n%d",*p,a,*q,b);
+//     return 0;
+// }
+
+//double pointer
+// #include<stdio.h>
+// int main()
+// {
+//     int a=1;
+//     int *p=&a;
+//     int **q=&p;
+//     int ***r=&q;
+//     printf("%d \n %d \n %d",*p,**q,***r);
+//     return 0;
+// }
+
+
